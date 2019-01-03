f20203f5448e17fcd77144daf75044beb1d9ea1b
fra <francesco.goinavi@gmail.com>
2019-01-03 13:16:43 +0100

    Change initial sizing

--- background.js
+++ background.js
@@ -7,8 +7,8 @@ chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create('index.html', {
         id : 'main',
         bounds : {
-            width : 620,
-            height : 500
+            width : 600,
+            height : 600
         }
     });
 });
